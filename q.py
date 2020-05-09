f78bc27fe59c316d2666ef34bd20000208a8e0c8
Par1Na <65071608+Par1Na@users.noreply.github.com>
2020-05-09 22:41:22 +0000

    Create q.py

---
+++ q.py
@@ -0,0 +1,12 @@
+# Twowaits
+Twowaits Problem
+Lname=input("Enter your name")
+age=(input("Enter your age"))
+gen=input("Enter your gender")
+college=input("Enter your college name")
+branch=input("Enter your branch")
+print(name)
+print(age)
+print(gen)
+print(college)
+print(branch)
