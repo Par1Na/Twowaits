12f2bbb3b7be972479f392d85bc32e8235f8e373
Par1Na <65071608+Par1Na@users.noreply.github.com>
2020-05-12 10:30:09 +0000

    Create q5.py

---
+++ q5.py
@@ -0,0 +1,17 @@
+# Twowaits
+Twowaits Problem
+run_a=int(input("runs scored by player 1 on 60 balls"))
+run_b=int(input("runs scored by player 2 on 60 balls"))
+run_c=int(input("runs scored by player 2 on 60 balls"))
+
+print("strike rate of player 1 is",(run_a/60)*100)
+print("strike rate of player 2 is",(run_b/60)*100)
+print("strike rate of player 3 is",(run_c/60)*100)
+
+print("runs scored after playing 60 more balls by player 1 is",2*run_a)
+print("runs scored after playing 60 more balls by player 2 is",2*run_b)
+print("runs scored after playing 60 more balls by player 3 is",2*run_c)
+
+print("maximum sixes hit by player 1",int(run_a/6))
+print("maximum sixes hit by player 2",int(run_b/6))
+print("maximum sixes hit by player 3",int(run_b/6))
