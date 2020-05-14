e619572cd79b8eb1660db2c316c69623e7d14dca
Par1Na <65071608+Par1Na@users.noreply.github.com>
2020-05-14 13:28:30 +0000

    Create day4-1.py

---
+++ day4-1.py
@@ -0,0 +1,27 @@
+# Twowaits
+Twowaits Problem
+
+# Program to count the number of times an element 
+# Present in the list 
+# Count function is used 
+
+def Count(tup, en): 
+
+    return tup.count(en) 
+
+  
+# Driver Code 
+
+tup = (10, 8, 5, 2, 10, 15, 10, 8, 5, 8, 8, 2) 
+
+enq = 4
+
+enq1 = 10
+
+enq2 = 8
+
+print(Count(tup, enq), "times") 
+
+print(Count(tup, enq1), "times") 
+
+print(Count(tup, enq2), "times") 
