41de254dc4660df92ee16f2738f609c6fa90e58f
Par1Na <65071608+Par1Na@users.noreply.github.com>
2020-05-16 12:19:31 +0000

    Create day5-1.py

---
+++ day5-1.py
@@ -0,0 +1,6 @@
+# Twowaits
+Twowaits Problem
+def converter(num):
+    return int(str(num).replace('0','5'))
+num=int(input("enter the number="))
+print("number after replacing 0 with 5=",converter(num))
